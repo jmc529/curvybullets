bd71ce27ace6de67a69607fe6c6773599b5cbec8
Hydrosaber <hydrosaber11@gmail.com>
2021-01-30 23:48:41 -0500

    Restored Bullet and added bullet shoot to gun.

---
+++ Assets/Scripts/GunShoot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour
+{
+    // transform of where the bullet should spawn
+    [SerializeField]
+    private Transform gunSpawnPoint;
+
+    // prefab for bullet to clone/instantiate
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private float rotationMultiplier;
+
+    // at what point should it register as a press
+    [SerializeField]
+    private float buttonTheshold;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float buttonPress = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+        // if the trigger is pressed
+        if(buttonPress >= buttonTheshold){
+            shoot();
+        }
+    }
+
+    void shoot(){
+        // get velocity and direction of aim
+        Vector3 handVelocity = OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RTouch);
+        float parallelScale = Vector3.Dot(handVelocity, gunSpawnPoint.forward);
+        // get perpendicular velocity
+        Vector3 perpVelocity = handVelocity - (parallelScale * gunSpawnPoint.forward);
+        // find scalar of vector
+        float rotationSpeed = perpVelocity.magnitude * rotationMultiplier;
+
+        //create a bullet and fire
+        GameObject newBullet = Instantiate(bulletPrefab, gunSpawnPoint);
+        newBullet.GetComponent<bullet>().SetRot(perpVelocity, rotationSpeed);
+    }
+}
